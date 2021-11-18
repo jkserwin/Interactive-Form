93ed77c7e34dc52bd29e2d98f1f607dd46796eb2
Jake Serwin <88554771+jkserwin@users.noreply.github.com>
2021-11-18 13:59:14 -0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Applies focus to Name input field on page load
+
+let nameInput = document.getElementById('name');
+nameInput.focus();
+
+// hides Job Role text input field by default 
+
+let jobRoleInput = document.getElementById('other-job-role');
+jobRoleInput.style.display = 'none';
+
+// Displays Job Role text input field when user selects "Other" from dropdown
+
+let jobRoleDropdown = document.getElementById('title');
+jobRoleDropdown.addEventListener("change", (e) => {
+    if (e.target.value = 'other') {
+        jobRoleInput.style.display = 'block';
+    } else if (e.target.value != 'other') {
+        jobRoleInput.style.display = 'none';
+    }
+})
